5296174c4138a6248372296a76f06743381f56fc
wbros <william.brossard@gmail.com>
2015-03-18 22:51:25 -0400

    merge

--- yis/Node.java
+++ yis/Node.java
@@ -19,7 +19,7 @@ class Node {
 
 	public Node getBestChild(int alpha, int beta) {
 		int val = 0;
-		for(Nove n : childs) {
+		for(Node n : childs) {
 			val = n.getBestChild(alpha, beta);
 		}
 		if (val != 0) {
@@ -33,10 +33,10 @@ class Node {
 			return val;
 	}
 
-	public void addChild(Nove child) { childs.add(child); }
+	public void addChild(Node child) { childs.add(child); }
 	public ArrayList<Node> getChilds() { return childs; }
-	public setMove(Move m) { move = m; }
+	public void setMove(Move m) { move = m; }
 	public Move getMove() { return move; }
-	public setValue(int v) { value = v; }
+	public void setValue(int v) { value = v; }
 	public int getValue() { return value; }
 }
